e12fa85bb1213e4cf790c64e02b940f24a474e30
Wasey Rahman <wasey123435@gmail.com>
2023-04-11 17:13:26 +0600

    Half part of Job Category

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
 import Header from './components/Header/Header'
+import List from './components/List/List'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,6 +13,7 @@ function App() {
     <div className="App">
       <Navbar></Navbar>
       <Header></Header>
+      <List></List>
       </div>
   )
 }
