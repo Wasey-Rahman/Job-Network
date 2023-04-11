d54e1856ac4f48b5885e0536c15cb6f829504919
Wasey Rahman <wasey123435@gmail.com>
2023-04-11 15:14:29 +0600

    Full Header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import headerImg from './header-image.jpg';
+const Header = () => {
+    return (
+        
+           
+
+
+    <header className='grid grid-cols-1 md:grid-cols-2 '>
+      <div className='flex items-center justify-center'>
+        <img src={headerImg} alt='Header' className='h-full w-full ' />
+      </div>
+      <div className='flex items-center justify-center p-8'>
+        <div>
+          <h1 className='text-4xl  font-bold mb-4'>One Step Closer To Your <span className='text-indigo-600'>Dream Job</span></h1>
+          <p className='text-sm mb-4 text-gray-700'>
+          Explore thousands of job opportunities with all the information you need. Its your future. Come find it. Manage all your job application from start to finish.
+          </p>
+          <button className='bg-indigo-600 text-white font-bold py-2 px-4 rounded'>
+          Get Started
+          </button>
+        </div>
+      </div>
+    </header>
+        
+    );
+};
+
+export default Header;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import Link from '../Link/Link';
+
+const Navbar = () => {
+    const routes = [
+        { id: 1, name: 'Statistics', path: '/Statistics' },
+        { id: 2, name: 'Applied Jobs', path: '/Applied Jobs' },
+        { id: 3, name: 'Blog', path: '/Blog' }
+        
+      ];
+    return (
+    
+    <nav>
+    <ul className='flex justify-center'>
+      <li className='font-bold text-lg px-6'>Job-Network</li>
+      {routes.map(route => (
+        <Link key={route.id} 
+        route={route} 
+        className={`mr-4 ${route.name === 'Statistics' ? 'text-indigo-600' : ''}`}
+        />
+      ))}
+      <li className='ml-auto mr-4'>
+        <button className=' bg-indigo-600 text-white font-bold py-2 px-4 rounded'>
+        Start Applying
+        </button>
+      </li>
+    </ul>
+  </nav>
+
+
+ 
+);
+};
+
+
+  
+       
+ 
+
+export default Navbar;
