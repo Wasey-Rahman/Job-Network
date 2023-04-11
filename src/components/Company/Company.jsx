3b9a16cb6177c99317951083ee07ac52fd37b5df
Wasey Rahman <wasey123435@gmail.com>
2023-04-12 01:42:59 +0600

    Full featured job

---
+++ src/components/Company/Company.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import './Company.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMapMarkerAlt, faDollarSign } from "@fortawesome/free-solid-svg-icons";
+
+const Company = (props) => {
+    const {logo,postName,companyName,jobType1,jobType2,location,salary,button} =props.Feature;
+    return (
+        <div>
+            <img src={logo} alt="" className='Image'/>
+            <h2 className='font-bold'>{postName}</h2>
+            <h4 className='text-gray-700'>{companyName}</h4>
+
+            <div className="flex gap-2">
+          <div className="border border-blue-600 rounded-md px-2 py-1">
+          <h6 className="text-indigo-700">{jobType1}</h6>
+          </div>
+         <div className="border border-blue-600 rounded-md px-2 py-1">
+         <h6 className="text-indigo-700">{jobType2}</h6>
+        </div>
+         </div>
+         
+        <div className="flex gap-4">
+        <div className="flex ">
+          <FontAwesomeIcon icon={faMapMarkerAlt} className="text-gray-400 mr-1" />
+          <h4 className="text-gray-700">{location}</h4>
+        </div>
+        <div className="flex gap-0 ">
+          <FontAwesomeIcon icon={faDollarSign} className="text-gray-400 mr-1" />
+          <h4 className="text-gray-700">{salary}</h4>
+        </div>
+      </div>
+
+      <div className="ml-auto">
+        <button className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 ">{button}</button>
+      </div>
+
+        </div>
+    );
+};
+
+export default Company;
--- src/components/Feature/Feature.jsx
+++ src/components/Feature/Feature.jsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Company from '../Company/Company';
+import './Feature.css'
 
 const Feature = () => {
+    const[Features,setFeatures]= useState([]);
+
+    useEffect(() => {
+        fetch('Features.json')
+        .then(res =>res.json())
+        .then(data =>setFeatures(data))
+    },[]);
+
     return (
         <section>
             <h1 className='text-4xl  font-bold mb-4 mt-20 text-center'>Featured Jobs</h1>
             <p className='text-sm mb-4 text-gray-700 text-center'>Job openings that are given special prominence by employers or job boards. They are typically highlighted and appear at the top of search results to attract more attention from job seekers.</p>
+            <div className='Feature-container'>
+            {
+            Features.map(Feature=><Company
+                key={Feature.id}
+                Feature={Feature}
+            ></Company>)
+           }
+            </div>
+            <div className="flex justify-center">
+                <button className='bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 mt-2'>See All Jobs</button>
+            </div>
         </section>
     );
 };
